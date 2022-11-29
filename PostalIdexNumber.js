65803fd3e1083bea8503fd8e983cfa167431b190
usha-12 <usha12ahirwar@gmail.com>
2022-11-29 14:14:35 +0530

    Talking alphabets or special character

--- PostalIdexNumber.js
+++ PostalIdexNumber.js
@@ -1,15 +1,15 @@
 function alphanumeric(str){
-    const regex = new RegExp(/^[0-9]+[a-z]/g);
+    const regex = new RegExp(/(?=.*[a-zA-Z\d].*)[a-zA-Z\d!@#$%&*]{7,}/g);
     if(str.match(regex)){
                 console.log("Its a numeric value");
     }else {
                 console.log("its not alphanumeric value");
     }
 }
-alphanumeric("400088");
-/*The Postal Index Number
-(PIN) or PIN Code is a 6
-digit code of Post Office
-numbering used
-by India Post.
-Create a regex pattern to*/
+alphanumeric("A400088");
+/*Restrict the PIN code from
+taking alphabets or special
+characters at the
+beginning.
+Check for A400088 – this
+should fail*/
