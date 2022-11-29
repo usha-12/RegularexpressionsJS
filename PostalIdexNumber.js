8e407abc98a8e73bf410497f1cf9b4cfa64ccb24
usha-12 <usha12ahirwar@gmail.com>
2022-11-29 14:22:09 +0530

    Make sure 400 088 is also valid along with 400088

--- PostalIdexNumber.js
+++ PostalIdexNumber.js
@@ -1,14 +1,11 @@
 function alphanumeric(str){
-    const regex = new RegExp(/(?=.*[a-zA-Z\d].*)[a-zA-Z\d!@#$%&*]{7,}/g);
+    const regex = new RegExp(/(?=.*[a-zA-Z\d].*)\s[a-zA-Z\d!@#$%&*]{7,}/g);
     if(str.match(regex)){
                 console.log("Its a numeric value");
     }else {
                 console.log("its not alphanumeric value");
     }
 }
-alphanumeric("400088B");
-/*Restrict the PIN code from
-taking alphabets or special
-characters at the End.
-Check for 400088B – this
-should fail*/
+alphanumeric("400 088B");
+/*Make sure 400 088 is also
+valid along with 400088*/
